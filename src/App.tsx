eca9257fc6308d05cc609c7f1325dfa8256f689e
Aditya Vashisht <adityavashisht34@gmail.com>
2025-05-23 13:20:14 +0530

    Bug Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -18,60 +18,59 @@ import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    // <AuthProvider>
-    //   <Router>
-    //     <Toaster position="top-right" />
-    //     <Routes>
-    //       <Route path="/" element={<Layout />}>
-    //         <Route index element={<Home />} />
-    //         <Route path="login" element={<Login />} />
-    //         <Route path="register" element={<Register />} />
+    <AuthProvider>
+      <Router>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
             
-    //         {/* Admin Routes */}
-    //         <Route path="admin" element={
-    //           <ProtectedRoute requiredRole="admin">
-    //             <AdminDashboard />
-    //           </ProtectedRoute>
-    //         } />
-    //         <Route path="admin/create-election" element={
-    //           <ProtectedRoute requiredRole="admin">
-    //             <CreateElection />
-    //           </ProtectedRoute>
-    //         } />
-    //         <Route path="admin/election/:id" element={
-    //           <ProtectedRoute requiredRole="admin">
-    //             <ManageElection />
-    //           </ProtectedRoute>
-    //         } />
+            {/* Admin Routes */}
+            <Route path="admin" element={
+              <ProtectedRoute requiredRole="admin">
+                <AdminDashboard />
+              </ProtectedRoute>
+            } />
+            <Route path="admin/create-election" element={
+              <ProtectedRoute requiredRole="admin">
+                <CreateElection />
+              </ProtectedRoute>
+            } />
+            <Route path="admin/election/:id" element={
+              <ProtectedRoute requiredRole="admin">
+                <ManageElection />
+              </ProtectedRoute>
+            } />
             
-    //         {/* Voter Routes */}
-    //         <Route path="voter" element={
-    //           <ProtectedRoute requiredRole="voter">
-    //             <VoterDashboard />
-    //           </ProtectedRoute>
-    //         } />
-    //         <Route path="voter/election/:id" element={
-    //           <ProtectedRoute requiredRole="voter">
-    //             <VotePage />
-    //           </ProtectedRoute>
-    //         } />
-    //         <Route path="voter/history" element={
-    //           <ProtectedRoute requiredRole="voter">
-    //             <VotingHistory />
-    //           </ProtectedRoute>
-    //         } />
+            {/* Voter Routes */}
+            <Route path="voter" element={
+              <ProtectedRoute requiredRole="voter">
+                <VoterDashboard />
+              </ProtectedRoute>
+            } />
+            <Route path="voter/election/:id" element={
+              <ProtectedRoute requiredRole="voter">
+                <VotePage />
+              </ProtectedRoute>
+            } />
+            <Route path="voter/history" element={
+              <ProtectedRoute requiredRole="voter">
+                <VotingHistory />
+              </ProtectedRoute>
+            } />
             
-    //         {/* Public Routes */}
-    //         <Route path="election/results/:id" element={<ElectionResults />} />
+            {/* Public Routes */}
+            <Route path="election/results/:id" element={<ElectionResults />} />
             
-    //         {/* Not Found and Redirects */}
-    //         <Route path="404" element={<NotFound />} />
-    //         <Route path="*" element={<Navigate to="/404" />} />
-    //       </Route>
-    //     </Routes>
-    //   </Router>
-    // </AuthProvider>
-    <div>App Under Maintenance, will be back soon</div>
+            {/* Not Found and Redirects */}
+            <Route path="404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
